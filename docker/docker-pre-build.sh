f1f52f3b2d91bee05ecb8c518969cc735f0fc50b
Jan Schnasse <Jan.Schnasse@dimdi.de>
2019-09-16 14:37:07 +0200

    move docker scripts to extra directory - ignore docker build dir 'svc'

---
+++ docker/docker-pre-build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $scriptdir
+cd ..
+/opt/activator-1.3.2-minimal/activator dist
+
+unzip -d docker/svc target/universal/*-1.0-SNAPSHOT.zip && mv docker/svc/*/* docker/svc/ && rm docker/svc/bin/*.bat && mv docker/svc/bin/* docker/svc/bin/start
+
+cd $scriptdir
+
+docker build -t skos-lookup .
+docker run -it -p 9000:9000 --rm skos-lookup
