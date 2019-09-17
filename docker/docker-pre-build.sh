bc63bd91b9d643c326c5d53ae67f09bac4e2ff27
Jan Schnasse <Jan.Schnasse@dimdi.de>
2019-09-17 13:38:57 +0200

    docker: add volume mount to persist elasticsearch data

--- docker/docker-pre-build.sh
+++ docker/docker-pre-build.sh
@@ -7,6 +7,6 @@ cd ..
 unzip -d docker/svc target/universal/*-1.0-SNAPSHOT.zip && mv docker/svc/*/* docker/svc/ && rm docker/svc/bin/*.bat && mv docker/svc/bin/* docker/svc/bin/start
 
 cd $scriptdir
-
+docker volume create skos-lookup-data
 docker build -t skos-lookup .
-docker run -it -p 9000:9000 --rm skos-lookup
+docker run --mount source=skos-lookup-data,target=/svc/data -it -p 9000:9000 --rm skos-lookup
